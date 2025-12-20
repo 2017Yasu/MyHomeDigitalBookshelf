edae998e5758e0c55327291eb5687dd7d7793dac
2017Yasu <yasu.nkym.96@gmail.com>
2025-12-20 23:22:05 +0900

    feat: add prettier

--- client/apps/web/src/App.tsx
+++ client/apps/web/src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import { greet } from "@mhdb/ui";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react';
+import { greet } from '@mhdb/ui';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
--- client/common/ui/src/index.ts
+++ client/common/ui/src/index.ts
@@ -1,3 +1,3 @@
 export function greet() {
-  console.log("Hello from UI package!");
+  console.log('Hello from UI package!');
 }
