93b98da11b1b814117e7f0142c069cb8d46e2cdd
2017Yasu <yasu.nkym.96@gmail.com>
2026-06-11 07:22:54 +0900

    chore: remove client and re-initialize speckit

--- client/common/ui/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-import AddBookForm from './AddBookForm';
-import BarcodeScanner from './BarcodeScanner';
-import BookList from './BookList';
-import Button from './Button';
-import FilterSort from './FilterSort';
-
-export { AddBookForm, BarcodeScanner, BookList, Button, FilterSort };
--- client/common/ui/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './components';
-export function greet() {
-  console.log('Hello from UI package!');
-}
